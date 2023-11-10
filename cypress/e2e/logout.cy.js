71906ffe2c7923192c77a1fe9e74059756964812
rydlande <rydlande@hotmail.com>
2023-11-10 23:14:37 +0100

    logout funtion works. e2e test

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 
-describe('User can log in and access their profile', () => {
-  it('logs user in', () => {
+describe('User can log out with the logout button', () => {
+  it('logs user out', () => {
     cy.visit('/', { setTimeout: 200 });
     cy.get('#registerModal', { timeout: 200 }).within(() => {
       cy.wait(500);
       cy.get(
         '.modal-dialog.modal-dialog.modal-dialog-centered.modal-dialog-scrollable',
-        { setTimeout: 1000 },
+        { setTimeout: 200 },
       ).within(() => {
         cy.get('form#registerForm').within(() => {
           cy.get('.modal-footer').children('.btn.btn-outline-success').click();
@@ -25,5 +25,8 @@ describe('User can log in and access their profile', () => {
         cy.get('.modal-footer').children('.btn.btn-success').click();
       });
     });
+    cy.wait(500);
+
+    cy.get('.btn.btn-outline-warning.me-2').click();
   });
 });
