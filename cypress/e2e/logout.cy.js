c1cfe16a6e9af2f109ffcc9027251c36ac3b8cf5
rydlande <rydlande@hotmail.com>
2023-11-13 14:35:38 +0100

    cypress tests

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -16,8 +16,8 @@ describe('User can log out with the logout button', () => {
     });
     cy.wait(500);
     cy.get('div#loginModal').within(() => {
-      cy.get('input[type="email"]').type(Cypress.env('USER_EMAIL'));
-      cy.get('input[type="password"]').type(Cypress.env('USER_PASSWORD'));
+      cy.get('input[type="email"]').type(Cypress.env('EMAIL'));
+      cy.get('input[type="password"]').type(Cypress.env('PASSWORD'));
     });
 
     cy.get('div#loginModal', { setTimeout: 1000 }).within(() => {
