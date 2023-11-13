a430167389b962f411444c6cac044d49565dde40
Eirin Rydland <112164731+rydlande@users.noreply.github.com>
2023-11-13 17:57:47 +0100

    Update invalid.cy.js

--- cypress/e2e/invalid.cy.js
+++ cypress/e2e/invalid.cy.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 describe('User cannot submit the login form with invalid credentials and is shown a message', () => {
-  it('should show error for invalid user', () => {
+  it('should show error for invalid user and/or wrong password', () => {
     cy.visit('/', { setTimeout: 200 });
     cy.get('#registerModal', { timeout: 200 }).within(() => {
       cy.wait(500);
@@ -16,14 +16,22 @@ describe('User cannot submit the login form with invalid credentials and is show
     });
     cy.wait(500);
     cy.get('div#loginModal').within(() => {
-      cy.get('input[type="email"]').type('invalid');
-      cy.get('input[type="password"]').type(Cypress.env('USER_PASSWORD'));
+      cy.get('input[type="email"]')
+        .invoke('attr', 'pattern')
+        .should('eq', '[\\w\\-.]+@(stud.)?noroff.no$');
+      cy.get('input[type="email"]').type('invalid@noroff.no');
+      cy.get('input[type="password"]').type(Cypress.env('PASSWORD'));
     });
 
     cy.get('div#loginModal', { setTimeout: 1000 }).within(() => {
       cy.get('form#loginForm').within(() => {
         cy.get('.modal-footer').children('.btn.btn-success').click();
       });
+    });
+    cy.on('window:alert', (message) => {
+      expect(message).to.equal(
+        `Either your username was not found or your password is incorrect`,
+      );
     });
   });
 });
