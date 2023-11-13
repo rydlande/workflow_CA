ac40cfe5383f30834bb7073b49a2e5d38a4d4730
Eirin Rydland <112164731+rydlande@users.noreply.github.com>
2023-11-13 18:01:25 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ export default defineConfig({
   e2e: {
     baseURL: 'https://rydlande.github.io/workflow_CA',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       return config;
     },
   },
